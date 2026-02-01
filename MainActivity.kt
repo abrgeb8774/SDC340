c9785080009f118413463902cd912b9379b1c587
unknown <abrgeb8774@students.ecpi.edu>
2026-02-01 04:11:57 -0500

    initial commit

---
+++ MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.abreham_gebremariam_contactsaddressbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+
+//Abreham Gebremariam
+//01/02/2026
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val alexanderButton = findViewById<Button> (R.id.alexanderButton)
+        alexanderButton.setOnClickListener {
+            val intent = Intent(this, ContactOneActivity::class.java)
+            startActivity(intent)
+        }
+        val adrianButton = findViewById<Button> (R.id.adrianButton)
+        adrianButton.setOnClickListener {
+            val intent = Intent(this, ContactTwoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
